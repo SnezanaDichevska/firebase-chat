463e8fa13b0167f37d3e0c7783a8594d66512f0e
WF | Snezhana Dichevska <snezhana.dichevska@webfactory.mk>
2016-12-18 17:23:32 +0100

    Added Firebase lib

--- src/app/_module/app.resolver.ts
+++ src/app/_module/app.resolver.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
+
 @Injectable()
 export class DataResolver implements Resolve<any> {
   constructor() {
